41239332bbe34f3285f74bce9f87f47b63d2c3e8
Andreas Birch <andreasbirch1998@hotmail.com>
2024-09-13 12:55:30 +0200

    Updated code to track only steps. Added data and plot

--- code.py
+++ code.py
@@ -12,9 +12,9 @@ from adafruit_bno08x import (
     BNO_REPORT_GYROSCOPE,
     BNO_REPORT_MAGNETOMETER,
     BNO_REPORT_ROTATION_VECTOR,
+    BNO_REPORT_STEP_COUNTER
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
-
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 bno = BNO08X_I2C(i2c)
 rtc = adafruit_ds3231.DS3231(i2c)
@@ -28,73 +28,53 @@ switch = DigitalInOut(board.A1)
 board_control = not switch.value
 print("Control of board is given to {}".format("board" if board_control else "computer"))
 
-tilt_switch = DigitalInOut(board.D5)
-tilt_switch.direction = Direction.INPUT
-tilt_switch.pull = Pull.UP
+tilt_switch_upward = DigitalInOut(board.D5)
+tilt_switch_upward.direction = Direction.INPUT
+tilt_switch_upward.pull = Pull.UP
 
-tilt_switch_state = tilt_switch.value
+tilt_switch_forward = DigitalInOut(board.D6)
+tilt_switch_forward.direction = Direction.INPUT
+tilt_switch_forward.pull = Pull.UP
+
+tilt_switch_upward_state = tilt_switch_upward.value
+tilt_switch_forward_state = tilt_switch_forward.value
 
 sleep_time = 0.1
 
 def get_datetime():
     timestamp = rtc.datetime
     return datetime(year=timestamp.tm_year, month=timestamp.tm_mon, day=timestamp.tm_mday, hour=timestamp.tm_hour, minute=timestamp.tm_min, second=timestamp.tm_sec)
-
+bno.enable_feature(BNO_REPORT_STEP_COUNTER)
 if board_control:
-    with open("/data_horizontal_fast.csv", "a+") as fp:
-        fp.write("timestamp,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,mag_x,mag_y,mag_z,quat_i,quat_j,quat_k,quat_real,switch_state,state_change\n")
+    with open("/data.csv", "a+") as fp:
+        fp.write("timestamp,bno_steps,forward_state_change,upward_state_change")
         while True:
             time.sleep(sleep_time)
             timestamp = get_datetime()
-            accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-            fp.write("%d,%0.6f,%0.6f,%0.6f," % (timestamp, accel_x, accel_y, accel_z))
-            
-            gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-            fp.write("%0.6f,%0.6f,%0.6f," % (gyro_x, gyro_y, gyro_z))
 
-            mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-            fp.write("%0.6f,%0.6f,%0.6f," % (mag_x, mag_y, mag_z))
+            #Check for state changes and update accordingly
+            upward_state_change = tilt_switch_upward.value != tilt_switch_upward_state
+            if upward_state_change:
+                tilt_switch_upward_state = tilt_switch_upward.value
 
-            quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-            fp.write("%0.6f,%0.6f,%0.6f,%0.6f," % (quat_i, quat_j, quat_k, quat_real))
-            
-            new_tilt_switch_state = tilt_switch.value
-            state_change = new_tilt_switch_state != tilt_switch_state
-            if(state_change):
-                tilt_switch_state = new_tilt_switch_state
-            fp.write("{0},{1}\n".format(tilt_switch_state, state_change))
+            forward_state_change = tilt_switch_forward.value != tilt_switch_forward_state
+            if forward_state_change:
+                tilt_switch_forward_state = tilt_switch_forward.value
 
+            fp.write("%s,%d,%s,%s" % (timestamp, bno.steps, upward_state_change, forward_state_change))
             fp.flush()
 else:
     while True:
         time.sleep(sleep_time)
-        print("Time")
-        print(get_datetime())
-        print("Acceleration:")
-        accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-        print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-        print("")
+        timestamp = get_datetime()
 
-        print("Gyro:")
-        gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-        print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-        print("")
+        #Check for state changes and update accordingly
+        upward_state_change = tilt_switch_upward.value != tilt_switch_upward_state
+        if upward_state_change:
+            tilt_switch_upward_state = tilt_switch_upward.value
 
-        print("Magnetometer:")
-        mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-        print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-        print("")
+        forward_state_change = tilt_switch_forward.value != tilt_switch_forward_state
+        if forward_state_change:
+            tilt_switch_forward_state = tilt_switch_forward.value
 
-        print("Rotation Vector Quaternion:")
-        quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-        print(
-            "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-        )
-        print("")
-
-        print("Tilt-switch:")
-        new_tilt_switch_state = tilt_switch.value
-        state_change = new_tilt_switch_state != tilt_switch_state
-        if(state_change):
-            tilt_switch_state = new_tilt_switch_state
-        print("State: {0} Change?: {1}".format(tilt_switch_state, state_change))
+        print("%s,%d,%s,%s" % (timestamp, bno.steps, upward_state_change, forward_state_change))
