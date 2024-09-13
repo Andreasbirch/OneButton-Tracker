9f551668c1876430d94b4217e996ac5d6cf58a33
Andreas Birch <andreasbirch1998@hotmail.com>
2024-09-13 10:47:04 +0200

    Added rtc

--- code.py
+++ code.py
@@ -4,7 +4,8 @@
 import time
 import board
 import busio
-import supervisor
+from adafruit_datetime import datetime
+import adafruit_ds3231
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_bno08x import (
     BNO_REPORT_ACCELEROMETER,
@@ -16,6 +17,7 @@ from adafruit_bno08x.i2c import BNO08X_I2C
 
 i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
 bno = BNO08X_I2C(i2c)
+rtc = adafruit_ds3231.DS3231(i2c)
 
 bno.enable_feature(BNO_REPORT_ACCELEROMETER)
 bno.enable_feature(BNO_REPORT_GYROSCOPE)
@@ -34,13 +36,16 @@ tilt_switch_state = tilt_switch.value
 
 sleep_time = 0.1
 
+def get_datetime():
+    timestamp = rtc.datetime
+    return datetime(year=timestamp.tm_year, month=timestamp.tm_mon, day=timestamp.tm_mday, hour=timestamp.tm_hour, minute=timestamp.tm_min, second=timestamp.tm_sec)
+
 if board_control:
     with open("/data_horizontal_fast.csv", "a+") as fp:
-        start_time = time.monotonic_ns()
         fp.write("timestamp,accel_x,accel_y,accel_z,gyro_x,gyro_y,gyro_z,mag_x,mag_y,mag_z,quat_i,quat_j,quat_k,quat_real,switch_state,state_change\n")
         while True:
             time.sleep(sleep_time)
-            timestamp = time.monotonic_ns() - start_time
+            timestamp = get_datetime()
             accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
             fp.write("%d,%0.6f,%0.6f,%0.6f," % (timestamp, accel_x, accel_y, accel_z))
             
@@ -63,6 +68,8 @@ if board_control:
 else:
     while True:
         time.sleep(sleep_time)
+        print("Time")
+        print(get_datetime())
         print("Acceleration:")
         accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
         print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
