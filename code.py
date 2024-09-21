88cfbc3da684afa3b7557fa71e50eb2568409a92
Andreas Birch <andreasbirch1998@hotmail.com>
2024-09-21 23:38:14 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,10 +1,10 @@
 import board
 from analogio import AnalogIn
 
-usb_voltage = AnalogIn(board.A0)
+usb_voltage = AnalogIn(board.A1)
 
 def get_voltage(pin):
-    return (pin * 3.3) / 65536 * 2
+    return (pin.value * 3.3) / 65536 * 2
 
 voltage_reading = get_voltage(usb_voltage)
-print(voltage_reading)
+print(voltage_reading > 3)
