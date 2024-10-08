bbf8676c00c901f37bd85efa5c9af6fe143c8776
Andreas Birch <andreasbirch1998@hotmail.com>
2024-10-08 13:31:01 +0200

    added typescript project

---
+++ app/src/FileUpload.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEventHandler } from "react"
+import { useState } from "react";
+import Papa from "papaparse"
+
+const FileUpload = () => {
+    const [fileJson, setFileJson] = useState(null);
+
+    const handleFileUpload = (e: any) => {
+        console.log(typeof(e));
+        const file = e.target.files[0];
+        if (file && file.type === "text/csv") 
+            parseCSV(file);
+        else {
+            alert("Den indsatte fil kunne ikke indlæses");
+        }
+    };
+
+    const parseCSV = (file: File) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            const csvData = reader.result;
+
+            Papa.parse(csvData, {
+                header: {},
+                skipEmptyLines: true,
+                complete: (result) => {
+                    setFileJson(result.data);
+                },
+                error: (error) => {
+                    console.error("Error parsing CSV: ", error);
+                },
+            });
+        };
+        reader.readAsText(file);
+    };
+
+    return <div>
+        <label>
+            Upload din fil her
+            <input type="file" accept=".csv" onChange={handleFileUpload} />
+            <pre>{JSON.stringify(fileJson, null, 2)}</pre>
+        </label>
+    </div>
+};
+
+export default FileUpload;
