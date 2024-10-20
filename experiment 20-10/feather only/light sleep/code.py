18cc712155d557095ac40c0b2028131971cb068d
Andreas Birch <andreasbirch1998@hotmail.com>
2024-10-20 13:39:01 +0200

    Cleanup

---
+++ experiment 20-10/feather only/light sleep/code.py
@@ -0,0 +1,10 @@
+import board
+from time import sleep
+from analogio import AnalogIn
+from logger import FileLogger, ConsoleLogger
+
+vbat_voltage = AnalogIn(board.VOLTAGE_MONITOR)
+usb_voltage = AnalogIn(board.A1)
+
+
+sleep(1000)
