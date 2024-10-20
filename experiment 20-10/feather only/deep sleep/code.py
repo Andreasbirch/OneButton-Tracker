0a64077b4414c14e68138d7125e701043124a5b4
Andreas Birch <andreasbirch1998@hotmail.com>
2024-10-20 16:55:57 +0200

    code cleanup

--- experiment 20-10/feather only/deep sleep/code.py
+++ experiment 20-10/feather only/deep sleep/code.py
@@ -1,54 +1,13 @@
+import alarm.time
 import board
-from time import sleep
+import alarm
+import time
 from analogio import AnalogIn
 from logger import FileLogger, ConsoleLogger
 
 vbat_voltage = AnalogIn(board.VOLTAGE_MONITOR)
 usb_voltage = AnalogIn(board.A1)
 
-def get_voltage(pin):
-    return (pin.value * 3.3) / 65536 * 2
-
-# logger = FileLogger("data.csv", ["Voltage"])
-logger = None
-
-usb_voltage_rea = get_voltage(usb_voltage)
-if get_voltage(usb_voltage) > 2:
-    logger = ConsoleLogger()
-else:
-    logger = FileLogger("test.csv", headers=[
-        "pre measurement"," post measurement"
-    ])
-
-
-## Calculate an average battery voltage to ensure no spikes
-def get_avg_voltage(pin, sample_size = 50):
-    avg = 0
-    for _ in range(sample_size):
-        avg += get_voltage(pin)
-    return avg / sample_size
-
-while True:
-    sample_size = 10
-    
-    average_pre_measurement_voltage = 0
-    average_post_measurement_voltage = 0
-
-    ## Make n samples, and take the average battery consumption of that
-    for i in range(sample_size):
-        ## Get battery reading before measurement
-        average_pre_measurement_voltage += get_avg_voltage(vbat_voltage)
-
-        ## Take measurement
-
-        ## Get battery reading after measurement
-        average_post_measurement_voltage += get_avg_voltage(vbat_voltage)
-
-    average_pre_measurement_voltage = average_pre_measurement_voltage / sample_size
-    average_post_measurement_voltage = average_post_measurement_voltage / sample_size
-
-    logger.log([
-        "{:.2f}".format(average_pre_measurement_voltage),
-        "{:.2f}".format(average_post_measurement_voltage)
-    ])
-    sleep(1)
+print("Going to sleep.")
+deep_sleep_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic()+1000)
+alarm.exit_and_deep_sleep_until_alarms(deep_sleep_alarm)
--- experiment 20-10/feather only/light sleep/code.py
+++ experiment 20-10/feather only/light sleep/code.py
@@ -6,5 +6,6 @@ from logger import FileLogger, ConsoleLogger
 vbat_voltage = AnalogIn(board.VOLTAGE_MONITOR)
 usb_voltage = AnalogIn(board.A1)
 
-
+print("Going to sleep.")
 sleep(1000)
+print("Woke up.")
