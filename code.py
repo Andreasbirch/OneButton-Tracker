41a331010db40eb49f8b421de937876bdcbbf3e3
Andreas Birch <andreasbirch1998@hotmail.com>
2024-11-20 11:11:32 +0100

    Working implementation

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
-import alarm.time
-import alarm.pin
-import alarm.touch
 import board
-import alarm
-import time
+from digitalio import DigitalInOut, Direction, Pull
+import alarm.pin
 
-print("Waking")
-tilt_switch_alarm = alarm.pin.PinAlarm(board.A0, value=True, edge=True, pull=True)
-# tilt_switch_alarm = alarm.touch.TouchAlarm(pin=board.A3)
-print(alarm.wake_alarm)
-alarm.exit_and_deep_sleep_until_alarms(tilt_switch_alarm)
+BUTTON = board.D9
+WAKEPIN = board.A0
+ 
+button = DigitalInOut(BUTTON)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+ 
+alarm.sleep_memory[0] = (alarm.sleep_memory[0] + 1) % 254
+print(alarm.sleep_memory[0], button.value, alarm.wake_alarm.value)
+
+# Create an alarm that will trigger if button on pin IO9 is pressed.
+pin_alarm = alarm.pin.PinAlarm(pin=WAKEPIN, value=not button.value, pull=False)
+ 
+# Light sleep until one of the defined alarms wake us
+reason = alarm.exit_and_deep_sleep_until_alarms(pin_alarm)
