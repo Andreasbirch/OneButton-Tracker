94952b6c240e64e41b1b114f9b73a9080939ba1e
Andreas Birch <andreasbirch1998@hotmail.com>
2025-01-06 21:10:07 +0100

    added button

--- visualizer-2/src/components/devices.tsx
+++ visualizer-2/src/components/devices.tsx
@@ -1,26 +1,45 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ipcRenderer } from 'electron';
 import { UsbDevice } from './../models/UsbDevice';
 import { Drive } from './../models/Drive';
-
+import { Button, Col, Container, Row } from 'react-bootstrap';
 ipcRenderer.send('available-devices-request', '');
 
 function Devices() {
     const [drives, setDrives] = useState<Drive[]>([]);
     const [usbDevices, setUsbDevices] = useState<UsbDevice[]>([]);
 
-    ipcRenderer.on('available-devices-broadcast', (e, args: {drives: Drive[], usbDevices: UsbDevice[]}) => {
-        console.log("Received devices from broadcast", args);
-        setDrives(args.drives);
-        setUsbDevices(args.usbDevices);
-    });
+     useEffect(() => {
+        const handleAvailableDevices = (e: Electron.IpcRendererEvent, args: { drives: Drive[], usbDevices: UsbDevice[] }) => {
+            console.log("Received devices from broadcast", args);
+            setDrives(args.drives);
+            setUsbDevices(args.usbDevices);
+        };
+        ipcRenderer.on('available-devices-broadcast', handleAvailableDevices);
+        ipcRenderer.send('available-devices-request', '');
+        
+
+    }, []); // Empty dependency array ensures this runs once on mount
 
     return <>
-        {
-            (!drives || drives.length === 0) ? <div> no devices found...</div> : drives.map(drive => {
-                return <div key={drive.path}>{drive.label} {drive.path}</div>
-            })
-        }
+        <Container>
+            <Row>
+                <Col>
+                {(!drives || drives.length === 0) ? 
+                    <Button className='btn btn-primary' style={{
+                        backgroundColor: 'lightgray',
+                        color: '#FFF',
+                        borderRadius: 10
+                    }}></Button> :
+                    <Button className='btn btn-primary' style={{
+                        backgroundColor: '#D44343',
+                        color: '#FFF',
+                        borderRadius: 10
+                    }}>{drives[0].label}</Button>
+                }
+                </Col>
+            </Row>
+        </Container>
     </>
 }
 
