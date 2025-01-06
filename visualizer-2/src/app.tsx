2932c6fe074c8254a616da84fd80a47ece963814
Andreas Birch <andreasbirch1998@hotmail.com>
2025-01-06 18:25:47 +0100

    Can parse data through main script and react component

--- visualizer-2/src/app.tsx
+++ visualizer-2/src/app.tsx
@@ -6,6 +6,13 @@ ipcRenderer.send('send-message', 'ping');
 function App() {
     return <div>
         <h1>React</h1>
+        <button type='button' onClick={() => {
+            console.log("Klik registreret");
+            ipcRenderer.send('get-usb-devices-request');
+            ipcRenderer.on('get-usb-devices-response', (e, data) => {
+                console.log("Received data", data);
+            });
+        }}>Tryk</button>
     </div>
 }
 
